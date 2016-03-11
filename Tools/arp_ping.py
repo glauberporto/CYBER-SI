6b413a4ab0535057f9ddc8133af0637a1b2c8ab8
Pedro Lopes <pedro.secinfo@gmail.com>
2016-03-10 22:41:16 -0300

    codes-exp-changes

--- Tools/arp_ping.py
+++ Tools/arp_ping.py
@@ -1,7 +1,13 @@
-#! /usr/bin/env python
+#!/usr/bin/env python2.7
 
 import sys
+import os
 from scapy.all import srp,Ether,ARP,conf
+
+user = os.getenv("SUDO_USER")
+if user is None:
+    print "This code needs 'sudo or be root'"
+    exit()
 
 if len(sys.argv) != 2:
         print "Usage: arp_ping <net> (e.g.,: arp_ping 192.168.1.0/24)"
@@ -16,4 +22,4 @@ print r"|       MAC        |        IP       |"
 print r"+------------------+-----------------+"
 for snd,rcv in ans:
         print rcv.sprintf(r" %Ether.src% | %ARP.psrc%")
-print r"+------------------+-----------------+" 
+print r"+------------------+-----------------+"
